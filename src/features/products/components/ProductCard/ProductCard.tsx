7268ceea6b4d0eb576ad6ffb682c07429ffcf452
Yana Karpovych <yanakarpovych0707@gmail.com>
2026-06-09 14:46:15 +0300

    updated product card layout

--- src/features/products/components/ProductCard/ProductCard.tsx
+++ src/features/products/components/ProductCard/ProductCard.tsx
@@ -2,34 +2,39 @@ import React from 'react';
 import './ProductCard.scss';
 import { Button } from '@base-ui/react';
 import { Icon } from '@/shared/components/Icon';
+import type { Product } from '../../types/Product';
 
-export const ProductCard: React.FC = () => {
+type Props = {
+  product: Product;
+}
+
+export const ProductCard: React.FC<Props> = ({ product }) => {
   return (
     <article className="product__card">
 
-      <img src="src/assets/img/category-phones.png" alt="Apple iPhone Xs 64GB Silver (iMT9G2FS/A)" className="product__card__image" />
+      <img src={product.image} alt={`${product.name} (iMT9G2FS/A)`} className="product__card__image" />
       
-      <h3 className="product__card__title">Apple iPhone Xs 64GB Silver (iMT9G2FS/A)</h3>
+      <h3 className="product__card__title">{`${product.name} (iMT9G2FS/A)`}</h3>
       
       <p className="product__card__price">
-        <span className="product__card__price-current">$799</span>
-        <span className="product__card__price-old">$899</span>
+        <span className="product__card__price-current">{`$${product.price}`}</span>
+        <span className="product__card__price-old">{`$${product.fullPrice}`}</span>
       </p>
 
       <section className="product__card__specs">
         <p className="product__card__spec">
           <span className="product__card__spec-label">Screen</span>
-          <span className="product__card__spec-value">5.8” OLED</span>
+          <span className="product__card__spec-value">{product.screen}</span>
         </p>
 
         <p className="product__card__spec">
           <span className="product__card__spec-label">Capacity</span>
-          <span className="product__card__spec-value">64 GB</span>
+          <span className="product__card__spec-value">{product.capacity}</span>
         </p>
 
         <p className="product__card__spec">
           <span className="product__card__spec-label">RAM</span>
-          <span className="product__card__spec-value">4 GB</span>
+          <span className="product__card__spec-value">{product.ram}</span>
         </p>
       </section>
 
