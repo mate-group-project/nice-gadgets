e2069839c024f50ecf937e2237bcff56dae17928
Андрій <andriystetsula2344gmail.com@MacBook-Pro-Andrij.local>
2026-06-09 14:11:23 +0300

    add breadcrumbs

--- src/features/favorites/api/favoritesApi.ts
+++ src/features/favorites/api/favoritesApi.ts
@@ -1,7 +1,7 @@
-import { client } from "@/shared/api/client";
-import type { Product } from "@/features/products/types/Product";
-import { ENDPOINTS } from "@/shared/api/endpoints";
+import { client } from '@/shared/api/client';
+import type { Product } from '@/features/products/types/Product';
+import { ENDPOINTS } from '@/shared/api/endpoints';
 
 export const favouritesApi = {
-    getAll: () => client.get<Product[]>(ENDPOINTS.favorites) 
+  getAll: () => client.get<Product[]>(ENDPOINTS.favorites),
 };
