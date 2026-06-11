14f8b3ff4b726f17ee226d6344cf65bfe71882ce
linska <linskatanya@gmail.com>
2026-06-11 14:18:42 +0300

    feat: add api and hooks for cart and favorites

---
+++ src/features/favorites/hooks/useFavoriteProducts.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import type { Product } from '@/features/products/types/Product.ts';
+import { getFavouriteProduct } from '@/features/favorites/api/favoritesApi.ts';
+
+export const useFavoriteProducts = () => {
+  const [favoritesProducts, setFavoritesProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const favoritesLocal = JSON.parse(
+      localStorage.getItem('favorites') || '[]',
+    );
+
+    if (favoritesLocal.length === 0) {
+      return;
+    }
+
+    const loadProductCart = async () => {
+      setIsLoading(true);
+      setError('');
+
+      try {
+        const result: { status: string; value?: Product }[] =
+          await Promise.allSettled(
+            favoritesLocal.map((id: string) => getFavouriteProduct(id)),
+          );
+
+        setFavoritesProducts(result.map((item) => item.value));
+      } catch {
+        setError('Product was not found');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadProductCart().then(() => {});
+  }, []);
+
+  return {
+    cartProducts: favoritesProducts,
+    isLoading,
+    error,
+  };
+};
