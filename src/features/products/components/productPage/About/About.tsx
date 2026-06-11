30a1f6156da6f149148fe70aec4777c2a940e188
Evgeniy45 <jekaolhovskii@gmail.com>
2026-06-11 10:16:04 +0300

    add type of peroduct details

--- src/features/products/components/productPage/About/About.tsx
+++ src/features/products/components/productPage/About/About.tsx
@@ -1,53 +1,26 @@
-import * as React from 'react';
+import type { ProductDescription } from '@/features/products/types/Product';
 import './about.scss';
 
-export const About: React.FC = () => {
+type AboutProps = {
+  productDescription: ProductDescription[];
+}
+
+export const About = ({ productDescription }: AboutProps) => {
   return (
-    <div className="product-page__about about">
+  <div className="product-page__about about">
       <h2 className="about__title">About</h2>
 
       <div className="about__content">
-        <article className="about__item">
-          <h3 className="about__subtitle">And then there was Pro</h3>
-          <p className="about__text about__history-text">
-            A transformative triple‑camera system that adds tons of capability
-            without complexity.
-          </p>
-          <p className="about__text">
-            An unprecedented leap in battery life. And a mind‑blowing chip that
-            doubles down on machine learning and pushes the boundaries of what a
-            smartphone can do. Welcome to the first iPhone powerful enough to be
-            called Pro.
-          </p>
-        </article>
+        {productDescription.map((desc, index) => (
+          <article key={index} className="about__item">
+            <h3 className="about__subtitle">{desc.title}</h3>
 
-        <article className="about__item">
-          <h3 className="about__subtitle">Camera</h3>
-          <p className="about__text">
-            Meet the first triple‑camera system to combine cutting‑edge
-            technology with the legendary simplicity of iPhone. Capture up to
-            four times more scene. Get beautiful images in drastically lower
-            light. Shoot the highest‑quality video in a smartphone — then edit
-            with the same tools you love for photos. You’ve never shot with
-            anything like it.
-          </p>
-        </article>
-
-        <article className="about__item">
-          <h3 className="about__subtitle">
-            Shoot it. Flip it. Zoom it. Crop it. Cut it. Light it. Tweak it.
-            Love it.
-          </h3>
-          <p className="about__text">
-            iPhone 11 Pro lets you capture videos that are beautifully true to
-            life, with greater detail and smoother motion. Epic processing power
-            means it can shoot 4K video with extended dynamic range and
-            cinematic video stabilization — all at 60 fps. You get more creative
-            control, too, with four times more scene and powerful new editing
-            tools to play with.
-          </p>
-        </article>
-      </div>
-    </div>
+            {desc.text.map((paragraph, i) => (
+              <p key={i} className="about__text">{paragraph}</p>
+            ))}
+          </article>
+        ))}
+      </div >
+    </div >
   );
 };
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -5,24 +5,39 @@ import { Gallery } from '../features/products/components/productPage/Gallery';
 import { ProductActions } from '../features/products/components/productPage/ProductActions';
 import { About } from '../features/products/components/productPage/About';
 import { TechSpecs } from '../features/products/components/productPage/TechSpecs';
+import { useParams } from 'react-router-dom';
+import { useProduct } from '@/features/products/hooks/useProduct';
 
 export const ProductPage: React.FC = () => {
+  const { productId } = useParams<{ productId: string }>();
+  const { product, isLoading, error } = useProduct(productId);
+
+  if (isLoading) {
+    return <div className="product-page__loading">Loading...</div>;
+  }
+
+  if (error || !product) {
+    return <div className="product-page__error">Error loading product details.</div>; 
+  }
+
+
+
   return (
     <section className="product-page">
       <div className="product-page__container">
         <h1 className="product-page__title">
-          Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)
+          {product.name}
         </h1>
 
         <div className="product-page__content">
           <div className="product-page__main-info">
-            <Gallery />
-            <ProductActions />
+            <Gallery images={product.images} />
+            <ProductActions product={product} />
           </div>
 
           <div className="product-page__details">
-            <About />
-            <TechSpecs />
+            <About productDescription={product.description} />
+            <TechSpecs product={product} />
           </div>
         </div>
       </div>
