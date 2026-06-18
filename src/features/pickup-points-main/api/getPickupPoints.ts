def92c21f95b4c28dfa6ccf5ea5559bc750e1f75
Yana Karpovych <yanakarpovych0707@gmail.com>
2026-06-18 11:45:23 +0300

    added logic for changing map language

--- src/features/pickup-points-main/api/getPickupPoints.ts
+++ src/features/pickup-points-main/api/getPickupPoints.ts
@@ -2,15 +2,15 @@ import { client } from '@/shared/api/client';
 import { ENDPOINTS } from '@/shared/api/endpoints';
 import type { PickupPoint } from '../types/PickupPoint';
 
-// type Language = 'en' | 'uk';
+type Language = 'en' | 'ua';
 
 type StoresResponse = {
   id: string;
   data: PickupPoint[];
 };
 
-export const getPickupPoints = async () => {
-  const response = await client.get<StoresResponse>(`${ENDPOINTS.stores}`);
+export const getPickupPoints = async (language: Language) => {
+  const response = await client.get<StoresResponse>(`${ENDPOINTS.stores}/${language}`);
 
   console.log('getPickupPoints response:', response);
 
